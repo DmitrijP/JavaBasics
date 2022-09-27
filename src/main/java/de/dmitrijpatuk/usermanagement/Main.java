e48c639352ff7eb1b3c3cd69b5d8df9fbe0a335d
Dmitrij Patuk <dmitrij.patuk@gmx.de>
2022-09-27 23:36:57 +0200

    Created new Class A

--- src/main/java/de/dmitrijpatuk/usermanagement/Main.java
+++ src/main/java/de/dmitrijpatuk/usermanagement/Main.java
@@ -5,4 +5,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
     }
+    
+    public static void a(){
+         System.out.println("Hello world!");
+    }
 }
