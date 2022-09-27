3af69b4332d06cd9490af5520922ecd67a400741
DmitrijP <dmitrij.patuk@gmx.de>
2022-09-27 23:38:24 +0200

    Added method a and ab to class main

--- src/main/java/de/dmitrijpatuk/usermanagement/Main.java
+++ src/main/java/de/dmitrijpatuk/usermanagement/Main.java
@@ -4,5 +4,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        a();
+        ab();
     }
+
+    public static void a(){
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+
+    public static void ab(){
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+
 }
