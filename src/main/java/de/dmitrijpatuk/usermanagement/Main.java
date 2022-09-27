f074548e542b39b2f95c727b21e9b777928ee8f9
DmitrijP <dmitrij.patuk@gmx.de>
2022-09-27 23:32:35 +0200

    Added User Class and three new Hello World Statements.

--- src/main/java/de/dmitrijpatuk/usermanagement/Main.java
+++ src/main/java/de/dmitrijpatuk/usermanagement/Main.java
@@ -3,5 +3,8 @@ package de.dmitrijpatuk.usermanagement;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
